24d474cf652e81e1bdea85d3bd20eaf2b5bc51f1
Johan <j.wennerberg@gmail.com>
2020-12-07 13:06:03 +0100

    Day 6 part 2

--- dec06.py
+++ dec06.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import re
+import collections
 
 def get_input(input_file):
     with open(input_file, "r") as f:
@@ -8,9 +8,16 @@ def get_input(input_file):
 
 def dec06():
     n_yes = 0
+    n_all_yes = 0
     for i in get_input("input/dec06"):
-        n_yes += len(set(i.replace("\n","")))
+        g = i.replace("\n", "")
+        n_yes += len(set(g))
+        answers = collections.Counter(list(g))
+        for k in answers:
+            if answers[k] == len(i.splitlines()):
+                n_all_yes += 1
     print(n_yes)
+    print(n_all_yes)
 
 
 if __name__ == '__main__':
